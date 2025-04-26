ad7095f6c154c7df922f3b9d95e405c70d169c77
taibarajabali <taibarajabali5@gmail.com>
2025-04-26 22:30:29 +0500

    Resolved merge conflicts

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require("dotenv").config();
 const express = require("express");
 const app = express();
@@ -20,27 +19,4 @@ app.get("/", (req, res) => {
 const port = process.env.PORT || 5000;
 
  app.listen(process.env.PORT, () => console.log(`Server running on port ${port}`));
-=======
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const port = 5000;
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const { connectDB } = require("./connection");
-const routes = require("./routes");
-
-connectDB();
-app.use(express.json());
-app.use(cors({ origin: "https://lovely-empanada-5f6eb2.netlify.app", credentials: true }));
-app.use(cookieParser());
-app.use("/api", routes);
-
-// 👇 Add this route for root URL
-app.get("/", (req, res) => {
-  res.send("Welcome to HomeChef Backend!");
-});
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
->>>>>>> c19c73637400f5f97d32a76c91f05618c6e1205d
 module.exports = app;
