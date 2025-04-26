9f8fd4231e7d90c8f272258fa0cd8ebb3a774612
taibarajabali <taibarajabali5@gmail.com>
2025-04-26 22:54:30 +0500

    Merge conflict resolved

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const jwt = require("jsonwebtoken");
 
 exports.verifyToken = (req, res, next) => {
@@ -15,22 +15,4 @@ exports.verifyToken = (req, res, next) => {
     req.userId = decoded.id;
     next();
   });
-=======
-const jwt = require("jsonwebtoken");
-
-exports.verifyToken = (req, res, next) => {
-  const token = req.cookies.token || req.header("Authorization")?.split(" ")[1];
-
-  if (!token) {
-    return res.status(401).json({ success: false, message: "Token is missing" });
-  }
-
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(403).json({ success: false, message: "Invalid token" });
-    }
-    req.userId = decoded.id;
-    next();
-  });
->>>>>>> c19c73637400f5f97d32a76c91f05618c6e1205d
 };
