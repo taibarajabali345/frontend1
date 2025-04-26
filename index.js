ebafde6ff8a1fd3dcb4e0d272f32f07e473b3930
taibarajabali <taibarajabali5@gmail.com>
2025-04-26 21:16:50 +0500

    changing

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const routes = require("./routes");
 
 connectDB();
 app.use(express.json());
-app.use(cors({ origin: "https://lovely-empanada-5f6eb2.netlify.app", credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(cookieParser());
 app.use("/api", routes);
 
