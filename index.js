48a5a0d319353c8fd62c7912420eb7ebd2b6071d
taibarajabali <taibarajabali5@gmail.com>
2025-04-26 19:47:41 +0500

    Update project

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const routes = require("./routes");
 
 connectDB();
 app.use(express.json());
-app.use(cors({ origin: "https://timely-faloodeh-924e95.netlify.app", credentials: true }));
+app.use(cors({ origin: "https://heroic-mousse-c67a4b.netlify.app", credentials: true }));
 app.use(cookieParser());
 app.use("/api", routes);
 
@@ -18,5 +18,8 @@ app.get("/", (req, res) => {
   res.send("Welcome to HomeChef Backend!");
 });
 
+// Listen (ONLY locally, Vercel will handle it on production)
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+// Export app for Vercel
 module.exports = app;
