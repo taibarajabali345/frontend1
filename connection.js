df1fd87d2f8f1ec5ca199b490dbb326ef3c2e0cf
taibarajabali <taibarajabali5@gmail.com>
2025-04-26 22:48:37 +0500

    Merge conflict resolved

--- connection.js
+++ connection.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
@@ -15,22 +15,10 @@ const connectDB = async () => {
   }
 };
 
-=======
-const mongoose = require("mongoose");
 
-const connectDB = async () => {
-  try {
-    console.log("Mongo URI: ", process.env.MONGO_URI);  // Add this line to debug
-    const connection = await mongoose.connect(process.env.MONGO_URI);
-    if (connection.connection.readyState === 1) {
-      console.log("MongoDB connected");
-    } else {
-      console.log("MongoDB connection failed");
-    }
-  } catch (error) {
-    console.log(error.message);
-  }
-};
 
->>>>>>> c19c73637400f5f97d32a76c91f05618c6e1205d
+
+
+
+
 module.exports = { connectDB };
