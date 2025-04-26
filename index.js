06f8d78c17f94175266d145fe1d1a2d0b17cdad8
taibarajabali <taibarajabali5@gmail.com>
2025-04-26 20:55:42 +0500

    Updated index.js and package.json

--- index.js
+++ index.js
@@ -6,15 +6,10 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const { connectDB } = require("./connection");
 const routes = require("./routes");
-const bcrypt = require('bcryptjs'); // <-- Add this line for bcrypt
 
 connectDB();
 app.use(express.json());
-const frontendUrl = process.env.NODE_ENV === 'production'
-  ? 'https://heroic-mousse-c67a4b.netlify.app'
-  : 'http://localhost:5173';
-
-app.use(cors({ origin: frontendUrl, credentials: true }));
+app.use(cors({ origin: "https://lovely-empanada-5f6eb2.netlify.app", credentials: true }));
 app.use(cookieParser());
 app.use("/api", routes);
 
