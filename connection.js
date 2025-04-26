4c3cc9b1bb4969ee5b2b0dfcf4d376c539d5a72c
taibarajabali <taibarajabali5@gmail.com>
2025-04-27 00:25:24 +0500

    Merge conflict resolved

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    console.log("Mongo URI: ", process.env.MONGO_URI);  // Debugging line
+    console.log("Mongo URI:", process.env.MONGO_URI);  // Debugging line
     const connection = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     
     if (connection.connection.readyState === 1) {
